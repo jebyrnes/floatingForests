7ada2c83f8f9224b0f2cc8b95481463dfef7cc4a
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2015-07-01 16:03:01 -0400

    Added more information on user behavior

--- r/userAnalysis.R
+++ r/userAnalysis.R
@@ -14,12 +14,25 @@ ffUsers <- kelpzoo %>% group_by(user_name) %>%
 
 #plot a histogram
 library(ggplot2)
-ggplot(data=ffUsers) + geom_bar(mapping=aes(x=n)) +
-  scale_x_log10()
+library(scales)
+
+options(scipen=999)#supress scientific notation
+pdf(file="../output/classifications_per_user_hist.pdf")
+ggplot(data=ffUsers) + geom_bar(mapping=aes(x=n), binwidth=.01) +
+  scale_x_log10(breaks = trans_breaks("log10", function(x) 10^x)) +
+  theme_minimal(base_size=17) +
+  xlab("Number of Classifications") +
+  ylab("Number of Users")
+dev.off()
 
 #treemap! from https://zoobackchannel.wordpress.com/2013/05/01/treemaps-of-volunteer-contributions/
 library(treemap)
-pdf(file="classifications_per_user.pdf",width=1024,height=768,pointsize=10)
+pdf(file="../output/classifications_per_user.pdf",width=128,height=96,pointsize=10)
 treemap(as.data.frame(ffUsers), index='user_name', vSize='n', vColor='user_name',
        type='index',fontsize.labels=0,lowerbound.cex.labels=1,title='')
 dev.off()
+
+
+#who are the top few?
+ffUsers %>% arrange(desc(n))
+ffUsers %>% filter(user_name=="jebyrnes")
