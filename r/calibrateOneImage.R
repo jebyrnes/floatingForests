9369452b4d8842c099804c9a0aa60a329a07276f
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2015-07-12 22:25:36 -0400

    Futzed with reprojection - no dice.

--- r/calibrateOneImage.R
+++ r/calibrateOneImage.R
@@ -2,11 +2,18 @@ library(R.matlab)
 source("./FFmethods.R")
 
 load("../data/oneImagesAKP00016e6.Rdata")
+#EPSG:3857?
+#CRS("+init=epsg:3857"))
 
+#polysData <- spTransform(polysData, "+proj=merc +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
+library(rgdal)
+writeOGR(polysData, "../data/", "AKP00016e6", driver="ESRI Shapefile")
 #load the SBCC data and reprocess into a SpatialPoints object
 caKelp <- readMat("../data/kelp_043035_7_24_1999.mat")
 caKelp <- caKelp$kelp[which(caKelp$kelp[,3]>0),]
-caKelp.spoints <- SpatialPoints(caKelp[,2:1])
+caKelp.spoints <- SpatialPoints(caKelp[,2:1], 
+                                proj4string=CRS(proj4string(polysData)))
+#caKelp.spoints <- spTransform(caKelp.spoints, CRS("+init=epsg:3857"))
 
 #get the bounds for plotting from the polygons data frame
 longBounds <- bbox(polysData)[1,]
