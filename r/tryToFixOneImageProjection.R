2c2ad09af2b275e472b929f1c4ab93d8655fb7d9
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2015-07-27 09:23:18 -0400

    Working with Tom's new coordinates

--- r/tryToFixOneImageProjection.R
+++ r/tryToFixOneImageProjection.R
@@ -16,6 +16,18 @@ oneImage$lower_left_y <- corners$ll[2]
 oneImage$upper_right_x <- corners$ur[1]
 oneImage$upper_right_y <- corners$ur[2]
 
+#FROM TOM
+#Upper Left: 36.315309, -122.000823
+#Lower Left: 36.217145, -122.002073
+#Upper Right: 36.314121, -121.867502
+#Lower Right: 36.215961, -121.868919
+
+oneImage$lower_left_x <- -122.002073
+oneImage$lower_left_y <- 36.217145
+oneImage$upper_right_x <- -121.868919
+oneImage$upper_right_y <- 36.314121
+
+
 #turn into a polygon
 polysData <- getSpatialPolysDataFrameForOneImage(oneImage, 
                                                  proj=proj)
@@ -35,7 +47,7 @@ plot(polysData, add=T)
 #load at the real coastline
 ####
 library(rgdal)
-coasts <- readOGR("../data/gshhg-shp-2.3.4/GSHHS_shp/l", layer="GSHHS_l_L1",  useC=FALSE)
+coasts <- readOGR("../data/gshhg-shp-2.3.4/GSHHS_shp/h", layer="GSHHS_h_L1",  useC=FALSE)
 
 #plot the image
 plotRGB(tileBrick)
